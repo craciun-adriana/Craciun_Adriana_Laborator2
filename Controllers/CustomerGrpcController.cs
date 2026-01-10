2a0eebbfabe865a558116782a0e89abe9c96d242
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2026-01-11 01:12:48 +0200

    Implement Grpc

---
+++ Controllers/CustomerGrpcController.cs
@@ -0,0 +1,66 @@
+﻿using Craciun_Adriana_Laborator2.Models;
+using Grpc.Net.Client;
+using GrpcCustomerService;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Craciun_Adriana_Laborator2.Controllers
+{
+    public class CustomerGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomerGrpcController()
+        {
+            //a se modifica portul corespunzator (vezi in proiectul GrpcCustomerService->Properties->launchSettings.json)
+            channel = GrpcChannel.ForAddress("https://localhost:7210");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GrpcCustomerService.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new CustomerService.CustomerServiceClient(channel);
+            GrpcCustomerService.Customer customer= client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Empty response = client.Delete(new CustomerId()
+            {
+                Id = id
+            });
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
